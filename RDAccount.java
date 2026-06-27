eb003519b8160afbdfec353efc3354c11a6cf573
Thilak-29 <thilakparamanathan2110@gmail.com>
2026-06-27 15:54:57 +0530

    RDAccount.java

--- RDAccount.java
+++ RDAccount.java
@@ -1,24 +1,26 @@
 package com.wipro.bank.acc;
 
-public class RDAccount extends Account{
-	public RDAccount(int Tenure, float Principle) {
-		this.Tenure = Tenure;
-		this.Principle = Principle;
-		
+public class RDAccount extends Account {
+
+	public RDAccount(int tenure, float principal) {
+		this.Tenure = tenure;
+		this.Principle = principal;
 	}
+	@Override
+
 	public float calculateAmountDeposited() {
-		int totalMonths = this.Tenure*12;
-		return this.Principle*totalMonths;
+		return Tenure * Principle*12;
 	}
+
+	@Override
 	public float calculateInterest() {
 		float totalDeposited = calculateAmountDeposited();
+        return (float) (totalDeposited * (Math.pow(1 + ((this.RateOfInterest / 100.0) / 4), 4 * this.Tenure) - 1));
 		
-		float interest = (totalDeposited*(this.RateOfInterest/100)*this.Tenure);
-		return interest;
 	}
+	
 	public float calculateMaturityAmount() {
-		 return calculateAmountDeposited() + calculateInterest();
-		
+	    return calculateAmountDeposited() + calculateInterest();
 	}
-}
 
+}
